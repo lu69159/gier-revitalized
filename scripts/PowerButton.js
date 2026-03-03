e8ca5a6b7aad98db929047be472e472f6357980b
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-03 12:15:16 +0700

    Create PowerButton.js

---
+++ scripts/PowerButton.js
@@ -0,0 +1,42 @@
+// Power button class
+var lastUpdate = 0
+
+Events.on(TapEvent, event => {
+    try {
+        
+        if(Vars.state.updateId == lastUpdate) return;
+
+        lastUpdate = Vars.state.updateId;
+        
+        const tile = event.tile;
+        if (!tile || !tile.build) return;
+
+        const target = Vars.content.getByName(ContentType.block, "gr-button");
+        const effect = Vars.content.getByName(ContentType.block, "gr-button-tap").generateEffect;
+
+        const block = tile.block();
+        if (block != target) return;
+
+        const build = tile.build;
+
+            Vars.ui.showInfoToast(build.team + " " + event.player.team(),1);
+        
+        if (build.team != event.player.team()) return;
+        if (build.power && build.power.graph) {
+            const pow = target.powerProduction * 60;
+
+            build.power.graph.transferPower(pow);
+        }
+
+        if (effect) {
+            effect.at(build.x, build.y);
+        }
+
+        if (Sounds.click) {
+            Sounds.click.at(build.x, build.y);
+        }
+    } catch (e) {
+        // fails silently on iOS instead of crashing
+        Vars.ui.showInfoToast("error: " + e,1);
+    }
+});
