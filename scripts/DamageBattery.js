2fcec025ec5286f49056b967b8216e0612148386
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-05 11:01:44 +0700

    Create DamageBattery.js

---
+++ scripts/DamageBattery.js
@@ -0,0 +1,43 @@
+
+Events.on(BuildDamageEvent, event => {
+
+try {
+
+const source = event.source;
+const build = event.build;
+
+if (!build || !source) return;
+    
+const block = event.build.block;
+const targetType = Vars.content.getByName(ContentType.block, "gr-power-cell");
+
+if (block == targetType){
+if (!build.power || !build.power.graph) return;
+if (build.power.graph.getBatteryStored() <= 0) return;
+    
+const damage = source.damage;
+const minus = damage * -1.75;
+const length = damage * 0.75;
+    
+build.power.graph.transferPower(minus);
+Fx.generate.at(build.x,build.y);
+    
+Vars.ui.showInfoToast(minus,1.5);
+
+Lightning.create(
+    build.team,
+    build.team.color,
+    damage,
+    build.x,
+    build.y,
+    Mathf.random(360),
+    length
+);
+    
+}
+    
+} catch(e){
+Vars.ui.showInfoToast("error: " + e,1);
+}
+    
+})
