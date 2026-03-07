5c16123d68df272d5f82112ffee4366aaba7420f
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-07 15:59:47 +0700

    Create BrittleDrill.js

---
+++ scripts/BrittleDrill.js
@@ -0,0 +1,44 @@
+// Damage Receiver
+Events.on(BuildDamageEvent, event => {
+try{
+const build = event.build;
+const source = event.source;
+const target = Vars.content.getByName(ContentType.block, "gr-ton-crusher");
+
+if (!build || !source) return;
+if (build.block != target || build.enabled == false || build.status() != BlockStatus.active) return;
+
+const damageExtra = source.damage * 2.75;
+build.damage(damageExtra);
+Fx.flakExplosion.at(source.x, source.y);
+
+} catch(e) {
+Vars.ui.showInfoToast(e,5);
+}
+})
+
+
+
+// On tap disabler
+Events.on(TapEvent, event => {
+try{
+const tile = event.tile;
+const player = event.player;
+
+if (tile.build == null) return;
+const block = tile.block();
+const build = tile.build;
+const target = Vars.content.getByName(ContentType.block,"gr-ton-crusher");
+
+    
+if (block == target){
+const enabled = build.enabled;
+build.enabled = !enabled;
+Sounds.click.at(build.x,build.y);
+
+}
+
+} catch(e) {
+Vars.ui.showInfoToast(e,4.5);
+}
+})
