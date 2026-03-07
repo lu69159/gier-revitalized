b112ac77b385de4e9922e17471df6d98b069b10e
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-07 22:21:18 +0700

    Update Startup.js

--- scripts/Startup.js
+++ scripts/Startup.js
@@ -14,9 +14,23 @@ Planets.gier.alwaysUnlocked = true;
 Planets.gier.clearSectorOnLose = true;
 
 Vars.maxSchematicSize = 3064;
-
+MapResizeDialog.maxSize = 2500;
+  
 Planets.gier.ruleSetter = r => {
 r.waveTeam = Team.blue;
 }
 
 });
+
+
+/// Startip dialog
+Events.on(ClientLoadEvent, e => { 
+try{
+const display = Core.bundle.get("mod.gr.display");
+const title = Core.bundle.get("mod.gr.mail");
+  
+Vars.ui.showText(title,display,Align.center);
+Vars.ui.showText("e",Vars.content.items().forEach(),Align.center);  
+} catch(e) {
+Vars.ui.showText("Not work",e,Align.center);
+}})
