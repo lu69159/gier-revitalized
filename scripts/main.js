511988b76e194641d872ae89bf27d804a3a28b9d
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-08 21:24:41 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,13 @@
 
 require("Startup");
 
-// Class Overrides
+//    ### Class Overrides ###
 
-require("PowerButton");
-require("DamageBattery");
-require("BrittleDrill");
+// Subclass: Block
+require("PowerButton"); // Requires to atleast have powerProduction
+require("DamageBattery"); // Requires to atleast be able to have comsumeBuffered
+require("BrittleDrill"); // Could actually be anything lol
+
+// Subclass: Units
+require("IFrameUnit");
+
