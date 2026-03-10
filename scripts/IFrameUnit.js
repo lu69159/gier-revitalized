bceddcf0f72b9f4e1a1ab09c4c2d3a5987582623
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-10 13:50:22 +0700

    Update IFrameUnit.js

--- scripts/IFrameUnit.js
+++ scripts/IFrameUnit.js
@@ -8,9 +8,10 @@ const valid = unit.getDuration(StatusEffects.invincible);
 
 if (damage < 10 || damage >= maxThres || unit.type != target || valid != 0) return;
 unit.apply(StatusEffects.invincible,25);
+unit.vel.set(0,0);
+  
 Fx.select.at(unit.x,unit.y);
 Sounds.unitCreateBig.at(unit.x,unit.y);
-
 } catch(e){
 Vars.ui.showInfoToast(e,3);
 }})
