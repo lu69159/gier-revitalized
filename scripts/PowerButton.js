8bd446b8dc2859babf754dc0f8adf794ea5e84ff
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-10 20:32:11 +0700

    Update PowerButton.js

--- scripts/PowerButton.js
+++ scripts/PowerButton.js
@@ -26,7 +26,7 @@ Events.on(TapEvent, event => {
             const pow = target.powerProduction * 60;
 
             build.power.graph.transferPower(pow);
-            build.power.graph.update();
+            build.updatePowerGraph();
         }
 
         if (effect) {
