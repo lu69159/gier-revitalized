9aad39be229148f6b9562fb14cc397a56b2c92cd
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-12 12:27:54 +0700

    Update IFrameUnit.js

--- scripts/IFrameUnit.js
+++ scripts/IFrameUnit.js
@@ -4,17 +4,18 @@ const unit = e.unit;
 const damage = e.bullet.damage;
 const targetBarracade = Vars.content.getByName(ContentType.unit, "gr-barracade");
 const targetAsh = Vars.content.getByName(ContentType.unit, "gr-ash");
+const status = e.bullet.type.status.name;
 const maxThres = (unit.maxHealth / 100) * 10;
 const valid = unit.getDuration(StatusEffects.invincible);
 
-if (damage < 10 || damage >= maxThres || valid != 0) return;
+if (damage < 10 || damage >= maxThres || valid != 0 || status != "gr-iframe-kill") return;
 if (unit.type != targetBarracade && unit.type != targetAsh) return;
   
 const dur = unit.type.crushDamage;
 
 unit.apply(StatusEffects.invincible,dur);
 unit.vel.set(0,0);
-  
+
 Fx.select.at(unit.x,unit.y);
 Sounds.unitCreateBig.at(unit.x,unit.y);
 } catch(e){
