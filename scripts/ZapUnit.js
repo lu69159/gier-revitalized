1e0067b93b9a39a0d67e632581e92f80f5471ad7
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-22 12:55:43 +0700

    Create ZapUnit.js

---
+++ scripts/ZapUnit.js
@@ -0,0 +1,24 @@
+Events.on(UnitDamageEvent, e => {
+try {
+
+const unit = e.unit;
+if (unit == null) return;
+
+const type = unit.type
+const target = Vars.content.getByName(ContentType.unit, "gr-electron");
+if (type != target) return;
+
+Sounds.shootArc.at(unit.x,unit.y);
+Lightning.create(
+    unit.team,
+    Color.valueOf("bccae0ff"),
+    35,
+    unit.x,
+    unit.y,
+    Mathf.random(360),
+    24 + Mathf.random(24)
+);
+    
+} catch(e){
+Vars.ui.showInfoToast(e,5);
+}});
