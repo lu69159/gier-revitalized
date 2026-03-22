734d2775c229b6b7a1a107004518d96d42117436
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-22 11:12:23 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ require("Startup");
 require("PowerButton"); // Requires to atleast have powerProduction
 require("DamageBattery"); // Requires to atleast be able to have consumeBuffered
 require("BrittleDrill"); // Could actually be anything lol
+require("LaunchSilo"); // Required to shootConne on 180 and needs to be a turret that can shoot
 
 // Subclass: Units
 require("IFrameUnit");
