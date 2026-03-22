c28f496af92e6f12822412c4faf7008ce8d67d70
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-22 11:11:10 +0700

    Create LaunchSilo.js

---
+++ scripts/LaunchSilo.js
@@ -0,0 +1,23 @@
+Events.on(TapEvent, event => {
+try{
+const tile = event.tile;
+const player = event.player;
+const block = tile.block();
+const build = tile.build;
+const silo = Vars.content.getByName(ContentType.block, "gr-launch-silo");
+    
+if (block == null || block != silo || build == null) return;
+const peekAmmo = build.peekAmmo();
+if (peekAmmo == null) return;
+    
+build.control(LAccess.shoot, build.x, build.y, 1, 0);
+Sounds.click.at(build.x,build.y);
+
+Timer.schedule(() => {  
+build.control(LAccess.shoot, build.x, build.y, 0, 0);
+}, 0.0166665);
+    
+} catch(e) {
+Vars.ui.showInfoToast(e,4.5);
+}
+});
