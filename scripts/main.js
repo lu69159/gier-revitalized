8e66fedf893dd592746d94cf8366566b46fe9cce
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-27 12:30:53 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,7 @@ require("PowerButton"); // Requires to atleast have powerProduction
 require("DamageBattery"); // Requires to atleast be able to have consumeBuffered
 require("BrittleDrill"); // Could actually be anything lol
 require("LaunchSilo"); // Required to shootConne on 180 and needs to be a turret that can shoot
+require("CompactCore"); // Adapted to CoreBlocl but could be used for anything. Spawner is a turret that kills itself
 
 // Subclass: Units
 require("IFrameUnit");
