3ade1b8b3697a2508c25f58d694a309b3d60a9b0
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-27 22:19:30 +0700

    Create Fabricator.js

---
+++ scripts/Fabricator.js
@@ -0,0 +1,26 @@
+Events.on(EventType.TapEvent, e => {
+try {
+const tile = e.tile;
+const block = tile.block();
+const target = Vars.content.getByName(ContentType.block, "gr-fabricator");
+
+if (block != target || tile.build == null) return;
+const sorted = tile.build.sorted;
+const build = tile.build;
+
+Sounds.click.at(build.x,build.y);
+    
+if (sorted == null || build == null) return;
+if(sorted instanceof UnitType) {
+sorted.spawn(tile.team(),build.x,build.y, build.rotation * 90);
+Fx.spawn.at(build.x,build.y);
+build.kill();
+
+} else if(sorted instanceof Block) {
+tile.setBlock(sorted,tile.team(),build.rotation);
+Fx.dooropenlarge.at(build.x,build.y);
+}
+    
+} catch(e){
+Vars.ui.showInfoToast(e,5);
+}});
