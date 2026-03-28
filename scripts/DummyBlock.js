4817e3deb47a50227ac02972b95fb232f64184d6
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-28 15:58:01 +0700

    Create DummyBlock.js

---
+++ scripts/DummyBlock.js
@@ -0,0 +1,26 @@
+Events.on(TapEvent, e => {
+try{
+const tile = e.tile;
+const block = tile.block();
+const target = Vars.content.getByName(ContentType.block,"gr-dummy");
+
+if (block != target || tile.build == null) return;
+
+const player = e.player;
+const team = player.team();
+const build = tile.build;
+const blockTeam = build.team;
+
+Sounds.click.at(build.x,build.y);
+Fx.select.at(build.x,build.y);
+    
+if (blockTeam != team){
+build.changeTeam(team);
+} else {
+build.changeTeam(Team.get(6));
+}
+
+     
+} catch(e){
+Vars.ui.showInfoToast(e,10);
+}});
