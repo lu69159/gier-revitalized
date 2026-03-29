c4105e1c0486bc05ac3da96be9d295a697125378
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-03-29 19:08:26 +0700

    Create TheStem.js

---
+++ scripts/TheStem.js
@@ -0,0 +1,40 @@
+Events.on(EventType.TapEvent, e => {
+    try{
+        const tile = e.tile;
+        if (!tile) return;
+
+        const build = tile.build;
+        if (!build) return;
+
+        const target = Vars.content.getByName(ContentType.block, "gr-the-stem");
+        if (tile.block() != target) return;
+
+        Sounds.click.at(build.x, build.y);
+
+        Vars.ui.showCustomConfirm(
+            "[green]The Stem[]",
+            "[#3a8f64]It's a green router[]",
+            "[red]Remove[]",
+            "[green]Spare[]",
+
+            () => {
+                const unit = Vars.content.getByName(ContentType.unit, "gr-router-god");
+                if (!unit){
+                    Vars.ui.showInfoToast("Unit null", 2);
+                    return;
+                }
+
+                //unit.spawn(Team.get(6), build.x, build.y,90);
+                Vars.ui.announce("[red]It know it's wrong and still commit.",4.5);
+                build.kill();
+            },
+
+            () => {
+                Vars.ui.showInfoToast("[#3a8f64]Wise choice.", 5);
+            }
+        );
+
+    } catch(err){
+        Vars.ui.showInfoToast(err + "", 5);
+    }
+});
