f2270b99ab6171b409d442e3e3848c785eee35ea
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-04 22:51:42 +0700

    Create MeltingPort.js

---
+++ scripts/MeltingPort.js
@@ -0,0 +1,75 @@
+// Extra stats
+Events.on(BlockInfoEvent, e => {
+try{
+Vars.ui.showInfoToast("open",10);
+
+const selectBlock = Vars.player.selectedBlock;
+const block = Vars.content.block("gr-melting-port");
+
+if (selectBlock != block) return;
+
+const cont = Vars.ui.content.cont;
+if (!cont) return;
+
+cont.row();
+cont.add("[accent]Recipes[]").left().row();
+cont.add().height(3).row();
+cont.image().color(Pal.accent).height(3).width(400).left().row();
+
+cont.add().height(6).row();
+cont.add("[#8c7fa9]Lead[lightgrey] -> [#8c7fa9]Liquid Lead[]").left().row();
+cont.add().height(6).row();
+cont.add("[#d99d73]Copper[lightgrey] -> [#d99d73]Liquid Copper[]").left().row();
+
+    
+} catch(e){
+Vars.ui.showInfoToast(e,10);
+}});
+
+var lastBuild = null;
+
+Events.on(TapEvent, e => {
+try{
+const tile = e.tile;
+const block = tile.block();
+const player = e.player;
+const build = tile.build;
+
+if (build != lastBuild){
+lastBuild = build;
+return;
+}
+
+
+// Multicrafter logic  
+if (player.team() != tile.team() || player.selectedBlock != null) return;
+
+const crafters = [
+Vars.content.block("gr-melting-port"),
+Vars.content.block("gr-lead-melting-port")
+];
+
+if (block == crafters[0] || block == crafters[1] || block == crafters[2]){
+Sounds.click.at(tile.worldx(),tile.worldy());
+}
+
+const buildTeam = build.team;
+const health = build.health;
+
+const blockTile = build.tile;
+if (block == crafters[0]){
+blockTile.setBlock(crafters[1], buildTeam);
+} else if (block == crafters[1]) {
+blockTile.setBlock(crafters[0], buildTeam);
+} else {
+return;
+}
+
+if (blockTile.block() != block){
+Fx.select.at(blockTile.worldx(),blockTile.worldy());
+blockTile.build.health = health;
+}
+  
+} catch(e){
+Vars.ui.showInfoToast(e,5);
+}});
