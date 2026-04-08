537193e019c11f7ed312d984d369a985df2ffd6e
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-08 13:54:21 +0700

    Update EnrageUnit.js

--- scripts/EnrageUnit.js
+++ scripts/EnrageUnit.js
@@ -2,13 +2,17 @@
 Events.on(UnitDamageEvent, e => {
 try{
 const unit = e.unit;
-const targetAsh = Vars.content.getByName(ContentType.unit, "gr-ash");
+const target = [
+Vars.content.unit("gr-ash"),
+Vars.content.unit("gr-arraign")
+];
+  
 const status = Vars.content.getByName(ContentType.status, "gr-rage");
 const maxThres = (unit.maxHealth / 2);
 const valid = unit.getDuration(status);
   
 if (unit.health >= maxThres || valid != 0) return;
-if (unit.type != targetAsh) return;
+if (unit.type != target[0] && unit.type != target[1]) return;
   
 unit.apply(status);
 
