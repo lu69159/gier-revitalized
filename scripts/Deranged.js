a4d5a17215f515e43aedef42a8cb1276e8e09d3e
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-08 11:46:20 +0700

    Create Deranged.js

---
+++ scripts/Deranged.js
@@ -0,0 +1,14 @@
+Events.on(WorldLoadEvent, () => {
+try{
+if (Core.settings.getBool("deranged") == true){        
+const rule = Vars.state.rules;
+rule.unitBuildSpeedMultiplier = 0.65
+rule.unitCrashDamageMultiplier = 1.5;
+rule.ghostBlocks = false;
+rule.deconstructRefundMultiplier = 0;
+rule.fog = true;
+rule.lighting = true;
+  
+}} catch(e){
+Vars.ui.showText("bruv",e);
+}});
