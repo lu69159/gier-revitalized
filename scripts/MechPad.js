a003af6ba6e5860fb0ec6493c7e729a9ca358eef
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-09 22:33:44 +0700

    Update MechPad.js

--- scripts/MechPad.js
+++ scripts/MechPad.js
@@ -3,7 +3,7 @@ try{
 const pad = Vars.content.block("gr-mechanical-pad");
   
 pad.addBar("progress", e => new Bar("Progress",Pal.techBlue, () => e && e. buildProgress > 0 ? e. buildProgress : 0 ));
-pad.stats.add(Stat.output, StatValue.content(pad.unitType));
+pad.stats.add(Stat.output, StatValues.content(pad.unitType));
               
 } catch(e){
 Vars.ui.showInfoText(e,10);
