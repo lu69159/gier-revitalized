952fefe74bb33452e5dcf3295143368b11ee5f97
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-10 17:37:25 +0700

    Update AttributeConstructor.js

--- scripts/AttributeConstructor.js
+++ scripts/AttributeConstructor.js
@@ -25,8 +25,8 @@ const offset = Math.round((size - 1) / 2);
 let totalAttribute = 0;
 let count = 0;
 
-for(let dx = 0; dx <= size; dx++){
-for(let dy = 0; dy <= size; dy++){
+for(let dx = 0; dx < size; dx++){
+for(let dy = 0; dy < size; dy++){
 
     const bx = building.tile.x - offset + dx;
     const by = building.tile.y - offset + dy;
