385ee679f9e66e43575bd37e5bc8e8f75b21a4c5
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-11 10:35:06 +0700

    Update DroneBay.js

--- scripts/DroneBay.js
+++ scripts/DroneBay.js
@@ -7,7 +7,6 @@ Events.on(ContentInitEvent, () => {
 try{
 
 const drones = new Stat("drones", StatCat.crafting);
-const buildTime = new Stat("buildTime", StatCat.crafting)
 
 function constructDroneBay(string){
     const block = Vars.content.block(string);
@@ -15,14 +14,10 @@ function constructDroneBay(string){
 
     if (!(block instanceof DroneCenter)) return;
 
-    if (block.droneType)
-        block.stats.add(Stat.output, StatValues.content(block.droneType));
+    if (block.droneType) block.stats.add(Stat.output, StatValues.content(block.droneType));
 
-    if (block.unitsSpawned != null)
-        block.stats.add(drones, block.unitsSpawned);
+    if (block.unitsSpawned != null) block.stats.add(drones, block.unitsSpawned);
 
-    if (block.droneConstructTime != null)
-        block.stats.add(buildTime, block.droneConstructTime / 60);
 }
   
 for (let i = 0; i < blocks.length; i++){
