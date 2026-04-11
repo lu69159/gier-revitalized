dcf200e4d5bd352b18760be1ae6cb217aabf4bab
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-11 12:34:11 +0700

    Update AttributeConstructor.js

--- scripts/AttributeConstructor.js
+++ scripts/AttributeConstructor.js
@@ -126,7 +126,7 @@ Vars.ui.showText("bruv",e);
 
 
 // Sets stats for the block
-Events.on(AtlasPackEvent, () =>{
+Events.on(SaveLoadEvent, () =>{
 try{  
 const block = Vars.content.block("gr-fissure-amalgam");
 block.stats.useCategories = true;
