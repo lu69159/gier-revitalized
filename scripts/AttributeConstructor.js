1228c88e51f461a9bde1a875cccb28abab492d8e
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-11 12:02:01 +0700

    Update AttributeConstructor.js

--- scripts/AttributeConstructor.js
+++ scripts/AttributeConstructor.js
@@ -127,7 +127,7 @@ Vars.ui.showText("bruv",e);
 
 // Sets stats for the block
 var initiated = false;
-Events.on(BlockInfoEvent, () =>{
+Events.on(ContentPatchLoadEvent, () =>{
 try{
 if (initiated == true) return;
 Vars.ui.showText("bruv","idk");
