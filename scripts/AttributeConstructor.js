c63f13803130ef303dd492d3bac5222c170f25ca
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-11 11:22:54 +0700

    Update AttributeConstructor.js

--- scripts/AttributeConstructor.js
+++ scripts/AttributeConstructor.js
@@ -123,7 +123,7 @@ Vars.ui.showText("bruv",e);
 
 
 // Sets stats for the block
-Events.on(ClientLoadEvent, () =>{
+Events.on(ModContentLoadEvent, () =>{
 try{
 const block =  Vars.content.block("gr-fissure-amalgam");
     
