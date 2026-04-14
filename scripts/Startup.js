8a52b8b10bac204b2d950964c6a40328af9b9cde
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-14 22:31:52 +0700

    Update Startup.js

--- scripts/Startup.js
+++ scripts/Startup.js
@@ -5,7 +5,7 @@ Events.on(ContentInitEvent, e => {
 Team.get(5).setPalette(Color.valueOf("8ecff5"));
 Team.get(5).emoji = "[#8ecff5][]"; 
 
-Team.get(4).setPalette(Color.valueOf("a66de3"));
+Team.get(4).setPalette(Color.valueOf("8f55ce"));
 Team.get(4).emoji = ""; 
 
 Planets.gier.visible = true;
