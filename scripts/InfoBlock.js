49f62eb102ab39b131ca64a7501fcdf24fbcee4d
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-14 12:12:41 +0700

    Update InfoBlock.js

--- scripts/InfoBlock.js
+++ scripts/InfoBlock.js
@@ -5,11 +5,11 @@ const build = tile.build;
 const block = tile.block();
 const target = Vars.content.block("gr-note");
 
-if (block != target || !build || Vars.player.selectBlock != null) return;
+if (block != target || !build || Vars.player.selectedBlock != null) return;
 const stringBuild = build.message;
 Vars.ui.showText("Note", stringBuild.toString());
 Sounds.click.play();
   
 } catch(e){
-Vars.ui.showInfoToast(e,15); 
+//Vars.ui.showInfoToast(e,15); 
 }});
