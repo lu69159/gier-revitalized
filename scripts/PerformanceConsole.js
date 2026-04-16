a55b5a22433f963cb21d4c8dcf787425e926612f
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-16 21:05:14 +0700

    Update PerformanceConsole.js

--- scripts/PerformanceConsole.js
+++ scripts/PerformanceConsole.js
@@ -1,57 +1,56 @@
 Events.on(TapEvent, e => {
-try{
-const tile = e.tile;
-const player = e.player;
-if (!tile || !player) return;
+    try{
+        const tile = e.tile;
+        const player = e.player;
+        if (!tile || !player) return;
 
-const block = tile.block();
-const console = Vars.content.block("gr-preformance-console");
-const buildTeam = tile.team();
-const playerTeam = player.team();
-const selectBlock = player.selectedBlock;
-if (block != console || selectBlock != null || buildTeam != playerTeam) return;
+        const block = tile.block();
+        const console = Vars.content.block("gr-preformance-console");
 
-Vars.ui.showMenu("@performance-console.title", "", 
-[
-["@performance-console.showStat"],
-["@performance-console.dedrone"]
-]
-, p => {
-try{  
-                
-if (p == 0){
-try{
-const stat = Vars.state.stats;
-const points = (stat.enemyUnitsDestroyed + stat.buildingsBuilt) * ((stat.wavesLasted * 0.45) + 1);
+        const buildTeam = tile.team();
+        const playerTeam = player.team();
+        const selectBlock = player.selectedBlock;
 
-Vars.ui.showText("@performance-console.option1.title",
-Core.bundle.format("performance-console.option1.enemyKill") + stat.enemyUnitsDestroyed + "\n" +
-Core.bundle.format("performance-console.option1.buildingsBuilt") + stat.buildingsBuilt + "\n\n" +
-Core.bundle.format("performance-console.option1.points") + points
-} catch(e){
-Vars.ui.showInfoToast(e + " - PreformanceConsole - [red]Inner",5); 
-}} else if (p == 1){
-try{
+        if (block != console || selectBlock != null || buildTeam != playerTeam) return;
 
-const facility = Vars.content.block("gr-drone-facility");
-const dropzone = Vars.content.block("gr-facility-dropzone");
-            
-Vars.ui.hudfrag.showToast(new TextureRegionDrawable(Core.atlas.find("gr-dedrone")), "[tan]Dedrone[red] Enabled");
-Vars.state.rules.bannedBlocks.add(facility);
-Vars.state.rules.bannedBlocks.add(dropzone);
+        Vars.ui.showMenu("@performance-console.title", "", 
+        [
+            ["@performance-console.showStat"],
+            ["@performance-console.dedrone"]
+        ], p => {
+            try{  
+                if (p == 0){
+                    const stat = Vars.state.stats;
+                    const points = (stat.enemyUnitsDestroyed + stat.buildingsBuilt) * ((stat.wavesLasted * 0.45) + 1);
 
-} catch(e){
-Vars.ui.showInfoToast(e + " - PerformanceConsole - [red]Inner",5); 
-}}
-);
+                    Vars.ui.showText("@performance-console.option1.title",
+                        Core.bundle.get("performance-console.option1.enemyKill") + stat.enemyUnitsDestroyed + "\n" +
+                        Core.bundle.get("performance-console.option1.buildingsBuilt") + stat.buildingsBuilt + "\n\n" +
+                        Core.bundle.get("performance-console.option1.points") + points
+                    );
 
+                } else if (p == 1){
 
-} catch(e){
-Vars.ui.showInfoToast(e + " - PerformanceConsole - [red]Inner",5); 
-}});
+                    const facility = Vars.content.block("gr-drone-facility");
+                    const dropzone = Vars.content.block("gr-facility-dropzone");
 
-} catch(e){
-Vars.ui.showInfoToast(e + " - PerformanceConsole",5); 
+                    Vars.ui.hudfrag.showToast(
+                        new TextureRegionDrawable(Core.atlas.find("gr-dedrone")),
+                        "[tan]Dedrone[red] Enabled"
+                    );
+
+                    if(facility) Vars.state.rules.bannedBlocks.add(facility);
+                    if(dropzone) Vars.state.rules.bannedBlocks.add(dropzone);
+                }
+
+            } catch(err){
+                Vars.ui.showInfoToast(err + " - PerformanceConsole - [red]Inner",5); 
+            }
+        });
+
+    } catch(err){
+        Vars.ui.showInfoToast(err + " - PerformanceConsole",5); 
+    }
 });
 
 /*
