770a5f793548e4c9797f5578237d51af6f618a62
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-16 12:28:44 +0700

    Update ReflectWall.js

--- scripts/ReflectWall.js
+++ scripts/ReflectWall.js
@@ -7,11 +7,11 @@ const blocks = [
 Events.on(BuildDamageEvent, e => {
 try{
 function deflect(bullet){
-bullet.type.create(e.build, bullet.x, bullet.y, bullet.rotation + 180);
+bullet.type.create(e.build, bullet.x, bullet.y, bullet.vel.angle() + 180);
 }
 
 for (let i = 0; i < blocks.length; i++){
-if (e.build.block.name == blocks[i]){
+if (e.build.block.name == blocks[i] && e.source != null){
 deflect(e.source);
 }}
 
