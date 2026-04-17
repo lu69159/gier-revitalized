4ebb7ccb2c8d7c23fdf5f431848fc7268c6b664a
lu69159 <2909165527@qq.com>
2026-04-17 23:42:42 +0800

    熔化器bundle

--- scripts/MeltingPort.js
+++ scripts/MeltingPort.js
@@ -12,16 +12,16 @@ const cont = Vars.ui.content.cont;
 if (!cont) return;
 
 cont.row();
-cont.add("[accent]Recipes[]").left().row();
+cont.add("@mpr.recipes").left().row();
 cont.add().height(3).row();
 cont.image().color(Pal.accent).height(3).width(400).left().row();
 
 cont.add().height(6).row();
-cont.add(" [#8c7fa9]Lead[lightgrey] -> [#8c7fa9]Liquid Lead[white]").left().row();
+cont.add("@mpr.recipes.1").left().row();
 cont.add().height(6).row();
-cont.add(" [#d99d73]Copper[lightgrey] -> [#d99d73]Liquid Copper[white]").left().row();
+cont.add("@mpr.recipes.2").left().row();
 cont.add().height(6).row();
-cont.add("  [#53565c]Silicon[lightgrey] -> [#53565c]Liquid Silicon[]").left().row();
+cont.add("@mpr.recipes.3").left().row();
 
     
 } catch(e){
