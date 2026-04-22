3262ebc7501c91128cc75468769cc8d03f6af14f
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-22 08:58:06 +0700

    Create DestructibleGen.js

---
+++ scripts/DestructibleGen.js
@@ -0,0 +1,24 @@
+const blocks = [
+"gr-steam-turbine"
+];
+
+Events.on(BuildDamageEvent, e => {
+try {
+const source = e.source;
+const build = e.build;
+const block = build.block;
+const valid = false;
+  
+for (let i = 0; i < blocks.length; i++){
+if (block == Vars.content.block(blocks[i]) valid = true;
+}
+
+if (valid != true || build.status != BlockStatus.active) return;
+
+let damage = (source.damage / 100);
+if (damage >= 0.35) damage = 35;
+build.instability += damage;
+
+} catch(e){
+Vars.ui.showInfoToast(e,5);  
+}});
