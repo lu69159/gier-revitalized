17166325b2ae35d5cc5aae4c331d7f1f65bee29e
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-26 11:04:29 +0700

    Update WreckFX.js

--- scripts/WreckFX.js
+++ scripts/WreckFX.js
@@ -6,10 +6,12 @@ const tile = e.tile;
 const block = tile.block();
 const build = tile.build;
 
+if (!build || !build.x || !build.y) return;
+  
 const particle = new ParticleEffect();
 Object.assign(particle, {
 particles: 1,
-cone: 15,
+cone: 0,
 cap: false,
 layer: 22,
 length: 0.1,
@@ -19,13 +21,20 @@ colorFrom: Color.valueOf("9f9f9f"),
 interp: Interp.sineIn,
 clip: 1000
 });
+
+var rotation = 0;
+if (block instanceof turret){
+rotation = build.rotation;
+} else {
+rotation = build.rotation * 90;
+}
   
 particle.region = String(block.uiIcon);
 particle.sizeFrom = particle.sizeTo = block.size * 3.95;
 particle.lifetime = Mathf.random(300,3000);
 particle.at(build.x, build.y);
 particle.baseLength = Mathf.random(-5,5);
-particle.offset = Mathf.random(-15,15) + (build.rotation * 90);
+particle.offset = Mathf.random(-15,15) + (rotation);
   
 } catch(e){
 Vars.ui.showInfoToast(e,5); 
