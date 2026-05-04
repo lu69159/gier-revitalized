21ea4c682a6fb3dff886138ed8abc3535f575842
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-04 21:00:31 +0700

    Update CircuitLogic.js

--- scripts/CircuitLogic.js
+++ scripts/CircuitLogic.js
@@ -12,10 +12,12 @@ Vars.ui.content.show(block);
 Vars.ui.content.hide();
   
 const rate = block.attributes.get(Attribute.get("circuitRate"));
+const heatDamage = block.attributes.get(Attribute.get("circuitHeatDamage"));
 const circuitUnit = statUnit.circuitUnit;
 
 block.stats.remove(stat.CircuitRate);
 block.stats.add(stat.CircuitRate, rate, circuitUnit);
+block.stats.add(stat.CircuitHeatDamage, heatDamage);
 }
   
 } catch(e){
