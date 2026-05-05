900cae0ea70f694d9b9df8395b049dcda3b8fb8b
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-05 17:10:36 +0700

    Update Attributes.js

--- scripts/Attributes.js
+++ scripts/Attributes.js
@@ -3,7 +3,8 @@
 const attributes = {
 healPercent: Attribute.add("healPerc"),
 circuitHeatDamage: Attribute.add("circuitHeatDamage"),
-circuitRate: Attribute.add("circuitRate")
+circuitRate: Attribute.add("circuitRate"),
+circuitRange: Attribute.add("circuitRange")
 }
 
 module.exports = attributes;
