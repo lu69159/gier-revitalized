8ce1f98869af28940f758616ec5e432a6f5bd3fe
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-05 17:11:14 +0700

    Update Stats.js

--- scripts/Stats.js
+++ scripts/Stats.js
@@ -2,7 +2,8 @@ const myStats = {
 Recipe: new Stat("recipes", StatCat.crafting),
 HealPercent: new Stat("healPerc", StatCat.function),
 CircuitRate: new Stat("circuitRate", StatCat.function),
-CircuitHeatDamage: new Stat("circuitHeatDamage", StatCat.function)
+CircuitHeatDamage: new Stat("circuitHeatDamage", StatCat.function),
+CircuitRange: new Stat("circuitRange", StatCat.function)
 };
 
 module.exports = myStats;
