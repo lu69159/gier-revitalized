69a158c47339a5110301c8f2b58303b76db37541
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-05 17:13:28 +0700

    Update CircuitLogic.js

--- scripts/CircuitLogic.js
+++ scripts/CircuitLogic.js
@@ -1,7 +1,7 @@
 const statUnit = require("StatUnits");
 const stat = require("Stats");
 // Blocks specifically Wires
-const blocks = ["gr-circuit-wire", "gr-circuit-splitter", "gr-power-cell", "gr-circuit-timer"];
+const blocks = ["gr-circuit-wire", "gr-circuit-splitter", "gr-power-cell", "gr-circuit-timer", "gr-signal"];
 // Blocks with special functions
 const other = ["gr-power-cell", "gr-circuit-splitter", "gr-circuit-timer"];
 
